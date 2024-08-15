c20b0af23b224f1b82018c213bb759c34f0cec3c
CoderGhost37 <coderghost37@gmail.com>
2024-08-15 12:51:43 +0530

    feat: authentication

--- src/app/(app)/documents/[id]/page.tsx
+++ src/app/(app)/documents/[id]/page.tsx
@@ -1,7 +1,24 @@
 import { Editor } from '@/components/editor/Editor';
+import { Header } from '@/components/header';
+import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs';
 
-export default async function DocumentPage() {
+const DocumentPage = () => {
   return (
-    <Editor />
+    <div className="">
+      <Header>
+        <div className="flex w-fit items-center justify-center">
+          <p className="document-title">Share</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
+      <Editor />
+    </div>
   )
 }
+
+export default DocumentPage
